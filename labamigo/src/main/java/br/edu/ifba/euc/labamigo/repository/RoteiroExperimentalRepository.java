0a25bece83e5efd8aa945e87ececec3b1d09eb7f
isabelasantosgoncalves <isabelasantosgoncalves15@gmail.com>
2026-06-14 23:03:52 -0300

    Delete labamigo directory

--- labamigo/src/main/java/br/edu/ifba/euc/labamigo/repository/RoteiroExperimentalRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifba.euc.labamigo.repository;
-
-import br.edu.ifba.euc.labamigo.model.RoteiroExperimental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RoteiroExperimentalRepository extends JpaRepository<RoteiroExperimental, Long> {
-
-}
